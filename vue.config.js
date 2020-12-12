c125fd21948005fc324849dac5a0949ca04bf8ab
racho91 <petyopenkov@hotmail.com>
2020-12-12 09:42:16 +0000

    vue install and confg

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // options...
+    devServer: {
+        disableHostCheck: true
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: '8081',
+        public: 'petyopenkov.com:8081',
+    },
+}
